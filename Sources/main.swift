fd815a489ce5fd53e029c8a9c31a7ef15b819b70
Bill Abt <babt@us.ibm.com>
2016-09-08 10:56:31 -0400

    Added Linux credentials path for testing.

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,11 +6,17 @@ HeliumLogger.use()
 
 let router = Router()
 
-let myCertPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/cert.pem"
-let myKeyPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/key.pem"
+// Gelareh's macOS credentials path...
+//let myCertPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/cert.pem"
+//let myKeyPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/key.pem"
 
+// Bill's macOS credentials path...
 //let myCertPath = "/Users/babt/Source/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
 //let myKeyPath = "/Users/babt/Source/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
+
+// Bill's Linux credentials path...
+let myCertPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
+let myKeyPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
 
 var mySSLConfig = SSLService.Configuration(withCACertificateDirectory: nil, usingCertificateFile: myCertPath, withKeyFile: myKeyPath)
 

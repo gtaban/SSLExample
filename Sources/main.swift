f6df32b07294a02333ddb013aafcd64fc6da598e
Gelareh Taban <gtaban@us.ibm.com>
2016-09-09 15:18:05 -0500

    Adding the SSL config that picks up the chain cert.

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,19 +6,33 @@ HeliumLogger.use()
 
 let router = Router()
 
+/*
 // Gelareh's macOS credentials path...
-//let myCertPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/cert.pem"
-//let myKeyPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/key.pem"
+let myCertPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/cert.pem"
+let myKeyPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/key.pem"
 
+    
 // Bill's macOS credentials path...
 //let myCertPath = "/Users/babt/Source/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
 //let myKeyPath = "/Users/babt/Source/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
-
+    
 // Bill's Linux credentials path...
-let myCertPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
-let myKeyPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
+//let myCertPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
+//let myKeyPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
 
-var mySSLConfig = SSLService.Configuration(withCACertificateDirectory: nil, usingCertificateFile: myCertPath, withKeyFile: myKeyPath)
+var mySSLConfig = SSLService.Configuration(withCACertificateDirectory: nil, usingCertificateFile: myCertPath, withKeyFile: myKeyPath, usingSelfSignedCerts: true)
+*/
+
+
+//#if HAVE_CA_CHAIN_CERT
+// Gelareh's macOS ca credentials path...
+let myCertChainFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/ca-chain.cert.pem"
+let myCertPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/server.cert.pem"
+let myKeyPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/server.key.pem"
+
+var mySSLConfig = SSLService.Configuration(withCACertificateFilePath: myCertChainFile, usingCertificateFile: myCertPath, withKeyFile: myKeyPath, usingSelfSignedCerts: false)
+
+
 
 mySSLConfig.cipherSuite = "ALL"
 
@@ -30,5 +44,6 @@ router.get("/") {
 
 Kitura.addHTTPServer(onPort: 8090, with: router, withSSL: mySSLConfig)
 Kitura.run()
+
 
 

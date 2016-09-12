c20718c0bccc3b6f523cf3b370b182cb45f661ea
Gelareh Taban <gtaban@us.ibm.com>
2016-09-12 16:18:21 -0500

    Add a pkcs12 self-signed cert reference SSLService 0.11.x to run AST on MacOS bump up swift-version to DEVELOPMENT-SNAPSHOT-2016-09-07-a

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SSLExample",
     dependencies: [
-        .Package(url: "https://github.com/gtaban/Kitura.git", Version(0, 28, 1)),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 15),
+        .Package(url: "https://github.com/gtaban/Kitura.git", majorVersion: 0, minor: 31),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 16),
     ])
 

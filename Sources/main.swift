e5ca3daef19e9b3ea5373aa0f5fb3b088153f2bf
Gelareh Taban <gtaban@us.ibm.com>
2016-09-14 15:27:18 -0500

    Clean up main and integrate the new Kitura SSLService.Configuration wrapper class.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,50 +1,64 @@
 import Kitura
-import SSLService
 import HeliumLogger
+
+#if os(Linux)
+    func TestSelfSignedLinux() -> SSLConfig {
+        // Gelareh's Self-signed macOS credentials path...
+        let myCertPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/cert.pem"
+        let myKeyPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/key.pem"
+        
+        // Bill's Self-signed macOS credentials path...
+        //let myCertPath = "/Users/babt/Source/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
+        //let myKeyPath = "/Users/babt/Source/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
+        
+        // Bill's Linux credentials path...
+        //let myCertPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
+        //let myKeyPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
+        
+        return SSLConfig(withCACertificateDirectory: nil, usingCertificateFile: myCertPath, withKeyFile: myKeyPath, usingSelfSignedCerts: true)
+    }
+    
+    func  TestCertChainLinux() -> SSLConfig {
+        // Gelareh's macOS ca credentials path...
+        let myCertChainFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/ca-chain.cert.pem"
+        let myCertFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/server.cert.pem"
+        let myKeyFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/server.key.pem"
+        
+        return SSLConfig(withCACertificateFilePath: myCertChainFile, usingCertificateFile: myCertFile, withKeyFile: myKeyFile, usingSelfSignedCerts: false)
+        
+    }
+#else
+    func TestSelfSignedMac() -> SSLConfig {
+        let myCertChainFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/cert.pfx"
+        
+        return SSLConfig(withChainFilePath: myCertChainFile, withPassword:"password", usingSelfSignedCerts:true)
+    }
+    
+    func TestCertChainMac() -> SSLConfig {
+        let myCertChainFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/server.cert.pfx"
+        
+        return SSLConfig(withChainFilePath: myCertChainFile, withPassword:"password", usingSelfSignedCerts:true)
+    }
+    
+#endif // os(Linux)
+
 
 HeliumLogger.use()
 
 let router = Router()
-
-/*
- 
-// Gelareh's macOS credentials path...
-let myCertPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/cert.pem"
-let myKeyPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/key.pem"
-
-    
-// Bill's macOS credentials path...
-//let myCertPath = "/Users/babt/Source/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
-//let myKeyPath = "/Users/babt/Source/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
-    
-// Bill's Linux credentials path...
-//let myCertPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/cert.pem"
-//let myKeyPath = "/mnt/hgfs/IBMSwift/Open Source/SSLExample/Creds/Self-Signed/key.pem"
-
-var mySSLConfig = SSLService.Configuration(withCACertificateDirectory: nil, usingCertificateFile: myCertPath, withKeyFile: myKeyPath, usingSelfSignedCerts: true)
-
-*/
-/*
-//#if HAVE_CA_CHAIN_CERT
-// Gelareh's macOS ca credentials path...
-//let myCertChainFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/ca-chain.cert.pem"
-let myCertChainFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/ca.cert.pem"
-let myCertFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/intermediate.cert.pem"
-let myKeyFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/intermediate.key.pem"
-//let myCertPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/server.cert.pem"
-//let myKeyPath = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/CertCA/server.key.pem"
-
-var mySSLConfig = SSLService.Configuration(withCACertificateFilePath: myCertChainFile, usingCertificateFile: myCertFile, withKeyFile: myKeyFile, usingSelfSignedCerts: false)
-*/
-
-
-let myCertChainFile = "/Users/gtaban/Developer/SecureService/SSLExample/Creds/Self-Signed/cert.pfx"
-
-var mySSLConfig = SSLService.Configuration(withChainFilePath: myCertChainFile, usingSelfSignedCerts:true, withPassword:"password");
+var mySSLConfigSelfSigned: SSLConfig
+var mySSLConfigChain: SSLConfig
 
 #if os(Linux)
+    mySSLConfigSelfSigned = TestSelfSignedLinux()
+    mySSLConfigChain = TestCertChainLinux()
+
     mySSLConfig.cipherSuite = "ALL"
-#endif
+#else
+
+    mySSLConfigSelfSigned = TestSelfSignedMac()
+    mySSLConfigChain = TestCertChainMac()
+#endif // os(Linux)
 
 router.get("/") {
     request, response, next in
@@ -52,8 +66,12 @@ router.get("/") {
     next()
 }
 
-Kitura.addHTTPServer(onPort: 8090, with: router, withSSL: mySSLConfig)
+Kitura.addHTTPServer(onPort: 8090, with: router, withSSL: mySSLConfigSelfSigned)
+Kitura.addHTTPServer(onPort: 8080, with: router, withSSL: mySSLConfigChain)
 Kitura.run()
+
+
+
 
 
 
